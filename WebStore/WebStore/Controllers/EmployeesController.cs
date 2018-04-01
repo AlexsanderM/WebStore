1c9336d28b3162fff5a1838714f964a5c1f20dfe
malov.alexsander <malov.alexsander@yandex.ru>
2018-04-01 15:40:23 +0300

    Partial

---
+++ WebStore/WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly List<EmployeeView> _employeesView = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Sasha",
+                SurName = "Petrov",
+                Patronymic = "Vladimirovich",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Aaa",
+                SurName = "Bbbb",
+                Patronymic = "Cccc",
+                Age = 5
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(_employeesView);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var employee = _employeesView.FirstOrDefault(t => t.Id == id);
+
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+    }
+}
--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -3,44 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<EmployeeView> _employeesView = new List<EmployeeView>
-        {
-            new EmployeeView
-            {
-                Id = 1,
-                FirstName = "Sasha",
-                SurName = "Petrov",
-                Patronymic = "Vladimirovich",
-                Age = 22
-            },
-            new EmployeeView
-            {
-                Id = 2,
-                FirstName = "Aaa",
-                SurName = "Bbbb",
-                Patronymic = "Cccc",
-                Age = 5
-            }
-        };
         public IActionResult Index()
         {
-            return View(_employeesView);
+            return View();
         }
 
-        public IActionResult Details(int id)
+        public IActionResult Shop()
         {
-            var employee = _employeesView.FirstOrDefault(t => t.Id == id);
+            return View();
+        }
 
-            if (employee == null)
-                return NotFound();
-            
-            return View(employee);
+        public IActionResult ProductDetails()
+        {
+            return View();
         }
     }
 }
